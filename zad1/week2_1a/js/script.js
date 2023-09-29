27be0725b08a103b574d6cf9cc9dd64cda802659
ndani1ov <132368604+ndani1ov@users.noreply.github.com>
2023-09-29 14:41:25 +0300

    добавлены файлы css

--- zad1/week2_1a/js/script.js
+++ zad1/week2_1a/js/script.js
@@ -1,11 +1,10 @@
 function formatAge() {
     var age = document.getElementById("age").value;
     var result = document.getElementById("result");
-    if((age / 10) % 10 == 1) 
-        result.textContent = age + " лет";
-    else if(age % 10 > 4 || age % 10 == 0) 
-        result.textContent = age + " лет";
-    else if(age % 10 == 1) 
+    if(age % 10 === 1 && age % 100 !== 11) 
         result.textContent = age + " год";
-    else result.textContent = age + " года";
+    else if(age % 10 >= 2 && age % 10 <= 4 && (age % 100 < 10 || age % 100 >= 20)) 
+        result.textContent = age + " года";
+    else  
+        result.textContent = age + " лет";
 }
